f3e4d916e691ce80152eff54dbcb7a38b3dee18a
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-12-22 14:49:15 -0500

    Create q171.py

---
+++ q171.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def titleToNumber(self, columnTitle):
+        """
+        :type columnTitle: str
+        :rtype: int
+        """
+        # res = 0
+        # loop through columnTitle using i (len(columnTitle) - 1, -1, -1):
+        # if i == len(columnTitle) - 1: res += letterToNum[columnTItle[i]]
+        # else: res += letterToNum[columnTitle[i]] * ((len(columnTitle) - 1) - i))
+        # return res
+        letterToNum = {
+            "A": 1,
+            "B": 2,
+            "C": 3,
+            "D": 4,
+            "E": 5,
+            "F": 6,
+            "G": 7,
+            "H": 8,
+            "I": 9,
+            "J": 10,
+            "K": 11,
+            "L": 12,
+            "M": 13,
+            "N": 14,
+            "O": 15,
+            "P": 16,
+            "Q": 17,
+            "R": 18,
+            "S": 19,
+            "T": 20,
+            "U": 21,
+            "V": 22,
+            "W": 23,
+            "X": 24,
+            "Y": 25,
+            "Z": 26
+        }
+
+        res = 0
+        for i in range(len(columnTitle) - 1, -1, -1):
+            if i == len(columnTitle) - 1: res += letterToNum[columnTitle[i]]
+            else: res += letterToNum[columnTitle[i]] * ( 26 ** ((len(columnTitle) - 1) - i) )
+        return res
